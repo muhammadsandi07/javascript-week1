0fcf8a311f676dbb7bdcb63d1dbd5b09580f9d76
musamaya <muhammadsandimaulana07@gmail.com>
2024-11-29 15:23:59 +0700

    jawaban task javascript introduction 1

---
+++ soal1.js
@@ -0,0 +1,13 @@
+const product = {
+  name: 'kemeja pria',
+  rating: 1,
+  price: 12000,
+  isActive: true,
+  images: ['hitam', 'merah'],
+  description: '',
+  variants: {
+    name: 'warna',
+    options: ['hitam', 'merah'],
+  },
+  storeName: '',
+}
